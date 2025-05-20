0cb7bff26ea0d14f04bf0951f32b33f0316af2f3
Thilak Raj <thilakofficials@gmail.com>
2025-05-20 14:56:05 +0530

    first commit

---
+++ assignmnetservice/assignmnetservice/src/main/java/com/assignmentservice/assignmnetservice/AssignmnetserviceApplication.java
@@ -0,0 +1,13 @@
+package com.assignmentservice.assignmnetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmnetserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmnetserviceApplication.class, args);
+	}
+
+}
---
+++ assignmnetservice/assignmnetservice/src/test/java/com/assignmentservice/assignmnetservice/AssignmnetserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignmentservice.assignmnetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmnetserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
